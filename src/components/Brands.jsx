4eaaace32116add1be93f4c0093952e8dad1d86d
stalin-2026 <stalinrobert83@gmail.com>
2026-09-19 06:33:30 +0530

    Animation speed up

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,16 +1,40 @@
+
 import React from 'react';
 
-const brands = ['dell', 'lg', 'philips', 'samsung', 'whirlpool', 'haier', 'sony', 'panasonic', 'bosch', 'ifb'];
+const brands = [
+  'dell',
+  'lg',
+  'philips',
+  'samsung',
+  'whirlpool',
+  'haier',
+  'sony',
+  'panasonic',
+  'bosch',
+  'ifb',
+];
 
 export default function Brands() {
+  const duplicatedBrands = [...brands, ...brands];
+
   return (
     <section id="brands" className="top-brands">
-      <h2 className="section-title">Top <span>Brands</span></h2>
+      <h2 className="section-title">
+        Top <span>Brands</span>
+      </h2>
+
       <div className="brands-wrapper">
         <div className="brands-track">
-          {[...brands, ...brands].map((brand, i) => (
-            <div className="brand-item" key={i}>
-              <img src={`assets/${brand}.png`} alt={brand} />
+          {duplicatedBrands.map((brand, i) => (
+            <div className="brand-item" key={`${brand}-${i}`}>
+              <img
+                src={`/assets/${brand}.png`}
+                alt={`${brand} logo`}
+                width="140"
+                height="80"
+                loading="lazy"
+                decoding="async"
+              />
             </div>
           ))}
         </div>
